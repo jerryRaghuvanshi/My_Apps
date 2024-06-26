ca9826aaa5951635343d33069c6298c3f77c24d8
jerryRaghuvanshi <jay10125r@gmail.com>
2024-06-26 22:27:51 +0530

    kg to pounds converter

---
+++ Unitconverterapp/app/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.unitconverterapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputstring = editText.getText().toString();
+                double kilos = Double.parseDouble(inputstring);
+                double pounds = converter(kilos);
+                textView.setText(""+pounds+" pounds");
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public double converter(double kilos) {
+        return kilos * 2.20462;
+    }
+}
